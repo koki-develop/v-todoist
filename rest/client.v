f1965246fc931b96f8e50a76aa10c86474c7b6e3
koki-develop <kou.pg.0131@gmail.com>
2023-10-20 13:19:07 +0900

    Add http request method in rest client

--- rest/client.v
+++ rest/client.v
@@ -1,5 +1,11 @@
 module rest
 
+import net.http
+
+const (
+	base_url = 'https://api.todoist.com/rest'
+)
+
 pub struct Client {
 pub mut:
 	token string [required]
@@ -9,4 +15,19 @@ pub fn new(token string) Client {
 	return Client{
 		token: token
 	}
+}
+
+fn (c Client) new_request(method http.Method, pathname string, data string) http.Request {
+	url := rest.base_url + pathname
+	mut req := http.new_request(method, url, data)
+	req.add_header(http.CommonHeader.authorization, 'Bearer ${c.token}')
+
+	match method {
+		.post {
+			req.add_header(http.CommonHeader.content_type, 'application/json')
+		}
+		else {}
+	}
+
+	return req
 }
